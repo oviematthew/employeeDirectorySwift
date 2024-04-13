3cc994a961eb0d556529fc9f0cdedf0c960827c0
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2024-04-12 20:54:04 -0400

    first commit

---
+++ EmployeesDirectory/EmployeesDirectory/EmployeesDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesDirectoryApp.swift
+//  EmployeesDirectory
+//
+//  Created by Matthew Ovie Enamuotor on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
